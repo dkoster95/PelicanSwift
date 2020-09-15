d3d84646e4537b3af481073164beb5b911a0f775
Daniel Koster <danielkoster95@gmail.com>
2020-09-15 11:38:07 -0400

    updated package

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "Pelican",
     platforms: [.iOS(.v11),
                 .watchOS(.v5),
-                .macOS(.v10_10),
+                .macOS(.v10_12),
                 .tvOS(.v12)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
