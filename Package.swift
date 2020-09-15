08fbdfec36b6590e4c52e7fef9b1e4e1c1ccb1f0
Daniel Koster <danielkoster95@gmail.com>
2020-09-15 11:29:36 -0400

    SPA support

---
+++ Package.swift
@@ -0,0 +1,30 @@
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Pelican",
+    platforms: [.iOS(.v11),
+                .watchOS(.v5),
+                .macOS(.v10_10),
+                .tvOS(.v12)],
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "Pelican",
+            targets: ["Pelican"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "Pelican",
+            dependencies: [],
+            path: "Sources"),
+        .testTarget(
+            name: "PelicanhTests",
+            dependencies: ["Pelican"],
+            path: "Tests"),
+    ]
+)
